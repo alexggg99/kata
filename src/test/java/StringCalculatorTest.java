a0bd2234839423a0daeb2cf355447c33408a16ba
Alex <alexggg99@gmail.com>
2019-04-13 17:02:50 +0300

    some tests

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -17,4 +17,36 @@ public class StringCalculatorTest {
         assertEquals(0, stringCalculator.add("") );
     }
 
+    @Test
+    public void simplePlus() {
+        assertEquals(7, stringCalculator.add("4+4"));
+    }
+
+    @Test
+    public void complexPlus() {
+        assertEquals(7, stringCalculator.add("-4+4"));
+    }
+
+    @Test
+    public void simpleMinus() {
+        assertEquals(1, stringCalculator.add("4-3"));
+    }
+
+    @Test
+    public void testNegativeResult() {
+        assertEquals(1, stringCalculator.add("4-3"));
+    }
+
+    @Test
+    public void complexFormula() {
+        assertEquals(-8, stringCalculator.add("4-3+1-10"));
+    }
+
+    @Test
+    public void lettreStringAndNumber() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            stringCalculator.add("f,7");
+        });
+    }
+
 }
