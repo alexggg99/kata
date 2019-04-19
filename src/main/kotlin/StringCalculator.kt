59f7bc4462c0a5234fbb0691d5dd4f4f92a53c77
Alex <alexggg99@gmail.com>
2019-04-19 21:47:06 +0300

    commit 2

--- src/main/kotlin/StringCalculator.kt
+++ src/main/kotlin/StringCalculator.kt
@@ -1,3 +1,5 @@
+import java.util.*
+
 class StringCalculator {
     fun calculate(str: String): Double {
         var expr = parse(str)
@@ -7,28 +9,74 @@ class StringCalculator {
     fun parse(str: String): Expression {
         var expr = str.trim()
         throwIfIllegalArgs(expr)
-        var arr = expr.split("+", "-")
-        when {
-            expr.contains("+") -> return Plus(Number(arr.get(0).toDouble()), Number(arr.get(1).toDouble()))
-            expr.contains("-") -> return Minus(Number(arr.get(0).toDouble()), Number(arr.get(1).toDouble()))
+
+        var stack: Stack<Expression> = Stack();
+        for (element in expr.split(" ")) {
+            when {
+                element.isNum() -> {
+                    if(stack.isEmpty())
+                        stack.push(Number(element.toDouble()))
+                    else {
+                        var expression = stack.pop()
+                        if(expression !is Operator) throw java.lang.IllegalArgumentException()
+                        expression.setExpres2(Number(element.toDouble()))
+                        stack.push(expression)
+                    }
+
+                }
+                element.isExpression() -> {
+                    if(stack.isEmpty()) throw java.lang.IllegalArgumentException()
+                    var expression = element.toExpression(stack.pop(), null)
+                    stack.push(expression)
+                }
+                else -> throw java.lang.IllegalArgumentException()
+            }
         }
-        return Number(0.0)
+        return stack.pop()
     }
 
     private fun throwIfIllegalArgs(expr: String) {
-        var fobiddenSimbols = expr.filter { !it.isDigit() && !it.equals('+') && !it.equals('-') }
+        var fobiddenSimbols = expr.filter { !it.isDigit() && !it.equals('+') && !it.equals('-') && !it.isWhitespace()}
         if (fobiddenSimbols.length > 0) throw IllegalArgumentException()
     }
 
     fun eval(expr: Expression): Double = when(expr) {
         is Number -> expr.value
-        is Plus -> eval(expr.expr1) + eval(expr.expr2)
-        is Minus -> eval(expr.expr1) - eval(expr.expr2)
+        is Plus -> eval(expr.expr1!!) + eval(expr.expr2!!)
+        is Minus -> eval(expr.expr1!!) - eval(expr.expr2!!)
     }
 
 }
 
+interface Operator {
+    var expr1: Expression?
+    var expr2: Expression?
+    fun setExpres1(expres1: Expression) {
+        this.expr1 = expres1
+    }
+
+    fun setExpres2(expres2: Expression) {
+        this.expr2 = expres2
+    }
+}
 sealed class Expression
-data class Number(val value: Double): Expression()
-data class Plus(val expr1: Expression, val expr2: Expression): Expression()
-class Minus(val expr1: Expression, val expr2: Expression): Expression()
+data class Number(var value: Double): Expression()
+data class Plus(override var expr1: Expression?, override var expr2: Expression?): Expression(), Operator
+data class Minus(override var expr1: Expression?, override var expr2: Expression?): Expression(), Operator
+
+fun String.isNum(): Boolean {
+    return this.toIntOrNull() != null
+}
+
+
+fun String.isExpression(): Boolean {
+    return this.equals("+") || this.equals("-")
+}
+
+fun String.toExpression(expt1: Expression?, expr2: Expression?): Expression {
+    when {
+        this.contains("+") -> return Plus(expt1, expr2)
+        this.contains("-") -> return Minus(expt1, expr2)
+    }
+    return Number(1.0)
+}
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,8 +1,6 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.awt.geom.Ellipse2D;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 public class StringCalculatorTest {
@@ -16,7 +14,8 @@ public class StringCalculatorTest {
 
     @Test
     public void emptyString_returnZero() {
-        assertEquals(0, stringCalculator.calculate("") );
+        assertThrows(IllegalArgumentException.class, () -> stringCalculator.calculate("") );
+        assertThrows(IllegalArgumentException.class, () -> stringCalculator.calculate("?1="));
     }
 
     @Test()
@@ -25,20 +24,34 @@ public class StringCalculatorTest {
         assertThrows(IllegalArgumentException.class, () ->stringCalculator.calculate("11+p"));
         assertThrows(IllegalArgumentException.class, () ->stringCalculator.calculate("2."));
         assertThrows(IllegalArgumentException.class, () ->stringCalculator.calculate("$78"));
+        assertThrows(IllegalArgumentException.class, () ->stringCalculator.calculate("8 + 332-"));
+        assertThrows(IllegalArgumentException.class, () ->stringCalculator.calculate("8 +- 332"));
+        assertThrows(IllegalArgumentException.class, () ->stringCalculator.calculate("8 + --332"));
+        assertThrows(IllegalArgumentException.class, () ->stringCalculator.calculate("8 +332"));
     }
 
     @Test
     public void singlePlus() {
-        assertEquals(3, stringCalculator.calculate(" 1+2") );
-        assertEquals(2, stringCalculator.calculate("2+0") );
-        assertEquals(340, stringCalculator.calculate(" 8+332 ") );
+        assertEquals(3, stringCalculator.calculate(" 1 + 2") );
+        assertEquals(2, stringCalculator.calculate("2 + 0") );
+        assertEquals(340, stringCalculator.calculate(" 8 + 332 ") );
+        assertEquals(10, stringCalculator.calculate(" +8 + 2 ") );
+        assertEquals(6, stringCalculator.calculate(" +8 + -2 ") );
     }
 
     @Test
     public void singleMinus() {
-        assertEquals(-1, stringCalculator.calculate("1-2") );
-        assertEquals(9, stringCalculator.calculate("13-4") );
-        assertEquals(-6, stringCalculator.calculate("8-14 ") );
+        assertEquals(-1, stringCalculator.calculate(" 1 - 2") );
+        assertEquals(9, stringCalculator.calculate("13 - 4") );
+        assertEquals(-6, stringCalculator.calculate("8 - 14 ") );
+        assertEquals(22, stringCalculator.calculate("8 + +14 ") );
+        assertEquals(22, stringCalculator.calculate("+8 - -14 ") );
+    }
+
+    @Test
+    public void complexExpression() {
+        assertEquals(406, stringCalculator.calculate(" 1 - 2 + 4 - 7 - 77 - -10 + 22 - 0 + 455") );
+        assertEquals(61, stringCalculator.calculate("100 - 2 - 48 + 22 - 11") );
     }
 
 }
